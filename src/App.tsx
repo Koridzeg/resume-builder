4a2550a176d0384c3734afa61b85444e49869d4c
KoridzeG <giorgiqori@yahoo.com>
2023-02-03 15:07:06 +0400

    feat : setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+const Landing = lazy(() => import('./pages/Landing'))
+const WizardForm = lazy(() => import('./pages/WizardForm'))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />}></Route>
+        <Route path="/create" element={<WizardForm />}></Route>
+      </Routes>
+    </Router>
+
   );
 }
 
---
+++ src/pages/WizardForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WizardForm: React.FC = () => {
+    return (
+        <div>WizardForm</div>
+    )
+}
+
+export default WizardForm
